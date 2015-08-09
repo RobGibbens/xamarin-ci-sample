b88b84ecb13f88a440768eb048c1d9fc01206051
Rob Gibbens <RobGibbens@gmail.com>
2015-08-09 09:48:36 -0400

    Updated Android to Xamarin.Forms 1.4.4

--- src/TipCalc.Android/MainActivity.cs
+++ src/TipCalc.Android/MainActivity.cs
@@ -6,21 +6,15 @@ using Xamarin.Forms;
 namespace TipCalc.Android
 {
     [Activity(Label = "TipCalc", MainLauncher = true)]
-    public class MainActivity : AndroidActivity
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
     {
-        protected override void OnCreate(Bundle bundle)
-        {
-            base.OnCreate(bundle);
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
 
-            Forms.Init(this, bundle);
+			global::Xamarin.Forms.Forms.Init (this, bundle);
 
-			Forms.ViewInitialized += (sender, e) => 
-			{
-				if (!string.IsNullOrWhiteSpace(e.View.StyleId))
-					e.NativeView.ContentDescription = e.View.StyleId;
-			};
-
-            SetPage(App.GetMainPage());
-        }
+			LoadApplication (new App ()); // method is new in 1.3
+		}
     }
 }
